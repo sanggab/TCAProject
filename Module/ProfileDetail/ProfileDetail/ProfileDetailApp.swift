72d073a24f1cb93b08ed6c3fb9248457b6fb9396
심상갑 <kappa2410@gmail.com>
2025-04-03 18:02:13 +0900

    Feat: TCA NavigationStackStore 공부

---
+++ Module/ProfileDetail/ProfileDetail/ProfileDetailApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileDetailApp.swift
+//  ProfileDetail
+//
+//  Created by Gab on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ProfileDetailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Module/ProfileEdit/ProfileEdit/ProfileEditApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileEditApp.swift
+//  ProfileEdit
+//
+//  Created by Gab on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ProfileEditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
