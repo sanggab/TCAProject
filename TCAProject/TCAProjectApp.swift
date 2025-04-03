c45a1199eab7c32d6f42ec4787e5b9e1e294841b
심상갑 <kappa2410@gmail.com>
2025-04-03 17:42:07 +0900

    Initial Commit

---
+++ TCAProject/TCAProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAProjectApp.swift
+//  TCAProject
+//
+//  Created by Gab on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TCAProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
